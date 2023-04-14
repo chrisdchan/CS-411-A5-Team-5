6156b5b27e2f5d4ea13051cd32f0c442f26223f2
chrisdchan <christopherdchan02@gmail.com>
2023-04-14 14:09:30 -0400

    Fix styleing on landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 
+	var brain = '/brain.jpg';
+
 	const handleLoginClick = () => {
 		goto('/login');
 	};
@@ -10,40 +12,48 @@
 	};
 </script>
 
-<h2
-	class="
-        bg-slate-200
-        absolute top-20 left-20
-        text-6xl
-        font-light
-        "
->
-	Insert Company Slogan Here
-</h2>
-
 <div
-	class="flex flex-row justify-center items-center
-            h-screen bg-slate-200"
+	class="
+		h-screen w-screen
+		bg-slate-200
+"
 >
-	<button
-		on:click={handleLoginClick}
+	<h2
 		class="
-            h-12 w-40
-            bg-sky-300/100
-            border-white border-solid rounded-lg
-            hover:bg-sky-400/100
-            m-6"
+			absolute top-20 left-20
+			text-6xl
+			font-light
+			"
 	>
-		Login
-	</button>
-	<button
-		on:click={handleSignUpClick}
-		class="
-            h-12 w-40
-            border-slate-500 border-solid border-2 rounded-lg
-            hover:bg-slate-300
-            "
+		Insert Company Slogan Here
+	</h2>
+
+	<div
+		class="flex flex-row justify-center items-center
+				h-full w-6/12
+				"
 	>
-		Sign Up
-	</button>
+		<button
+			on:click={handleSignUpClick}
+			class="
+				h-12 w-40
+				bg-sky-300/100
+				border-white border-solid rounded-lg
+				hover:bg-sky-400/100
+				m-6
+				"
+		>
+			Sign Up
+		</button>
+		<button
+			on:click={handleLoginClick}
+			class="
+				h-12 w-40
+				border-slate-500 border-solid border-2 rounded-lg
+				hover:bg-slate-300
+				"
+		>
+			Login
+		</button>
+	</div>
 </div>
